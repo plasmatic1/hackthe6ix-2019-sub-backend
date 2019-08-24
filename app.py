4dcbb9e0ec012baee847219bb7555bf1dd7a66c1
Elytrus <moses@mosesxu.net>
2019-08-24 18:58:52 -0400

    Added errors for all URLs and fixed distance func

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import math
 
 from flask import Flask
 
+from dist_util import dist
 from id import valid_id, rem_id, add_id
-from packets import OutIdPacket, OutDistPacket
 
 app = Flask(__name__)
 
@@ -12,23 +12,9 @@ teams = {}
 locs = {}
 
 
-def dist(a, b):
-    """
-    Dist between two points (a and b)
-    :param a: Point A as a tuple (x, y)
-    :param b: Point B as a tuple (x, y)
-    :return: The distance (as a float)
-    """
-    da = a[0] - b[0]
-    db = a[1] - b[1]
-    return math.sqrt(da * da + db * db)
-
-
 def error(msg):
     print(f'[ERROR]: {msg}')
 
-
-# 261087034
 
 @app.route('/del/<int:id>')
 def del_user(id):
@@ -39,14 +25,14 @@ def del_user(id):
         print(f'Removed ID {id}')
         rem_id(id)
 
-    return ''
+    return json.dumps({'error': ''})
 
 
 @app.route('/add/')
 def add_user():
     new_id = add_id()
     print(f'Created user {new_id}')
-    return json.dumps({'id': new_id})
+    return json.dumps({'id': new_id, 'error': ''})
 
 
 @app.route('/loc/<int:id>/<float:lat>/<float:long>/')
@@ -65,7 +51,7 @@ def loc(id, lat, long):
                 best = min(best, dist(cloc, loc))
 
         print(f'Changed loc of ID {id} to {cloc}')
-        return json.dumps({'dist': best})
+        return json.dumps({'dist': best, 'error': ''})
 
 
 @app.route('/team/<int:id>/<int:team>')
@@ -76,7 +62,7 @@ def set_team(id, team):
     else:
         print(f'Set team of ID: {id} to {team}')
         teams[id] = team
-        return ''
+        return json.dumps({'error': ''})
 
 
 if __name__ == '__main__':
